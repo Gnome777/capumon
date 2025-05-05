a7b43874be492b2f9c588d04110cf0cf4bc5a725
Gnome67 <102388813+Gnome777@users.noreply.github.com>
2025-05-04 22:41:28 -0500

    gen 7 crit chances
    fixed more softlock issues

--- home/trainers.asm
+++ home/trainers.asm
@@ -135,9 +135,9 @@ IF DEF(_DEBUG)
 	call DebugPressedOrHeldB
 	jr nz, .trainerNotEngaging
 ENDC
-	ld a, [wPartyCount]
-	and a
-	jp z, .trainerNotEngagin
+	; ld a, [wPartyCount]
+	; and a
+	; jp z, .trainerNotEngaging
 	call CheckForEngagingTrainers
 	ld a, [wSpriteIndex]
 	cp $ff
