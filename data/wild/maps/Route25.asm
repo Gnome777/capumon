f91df208b93e7eda34d7b33d8edf04f94bd86a0a
Gnome67 <102388813+Gnome777@users.noreply.github.com>
2025-05-07 22:54:46 -0500

    rework pokemon spawns 1
    removed version exclusives and reworked fishing pokemon

--- data/wild/maps/Route25.asm
+++ data/wild/maps/Route25.asm
@@ -1,6 +1,5 @@
 Route25WildMons:
 	def_grass_wildmons 15 ; encounter rate
-IF DEF(_RED)
 	db  8, WEEDLE
 	db  9, KAKUNA
 	db 13, PIDGEY
@@ -11,19 +10,6 @@ IF DEF(_RED)
 	db 10, ABRA
 	db  7, METAPOD
 	db  8, CATERPIE
-ENDC
-IF DEF(_BLUE)
-	db  8, CATERPIE
-	db  9, METAPOD
-	db 13, PIDGEY
-	db 12, BELLSPROUT
-	db 13, BELLSPROUT
-	db 12, ABRA
-	db 14, BELLSPROUT
-	db 10, ABRA
-	db  7, KAKUNA
-	db  8, WEEDLE
-ENDC
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
--- data/wild/maps/Route8.asm
+++ data/wild/maps/Route8.asm
@@ -1,7 +1,6 @@
 Route8WildMons:
 	def_grass_wildmons 15 ; encounter rate
 	db 18, PIDGEY
-IF DEF(_RED)
 	db 18, MANKEY
 	db 17, EKANS
 	db 16, GROWLITHE
@@ -11,18 +10,6 @@ IF DEF(_RED)
 	db 17, GROWLITHE
 	db 15, GROWLITHE
 	db 18, GROWLITHE
-ENDC
-IF DEF(_BLUE)
-	db 18, MEOWTH
-	db 17, SANDSHREW
-	db 16, VULPIX
-	db 20, PIDGEY
-	db 20, MEOWTH
-	db 19, SANDSHREW
-	db 17, VULPIX
-	db 15, VULPIX
-	db 18, VULPIX
-ENDC
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
