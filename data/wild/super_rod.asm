d8d91c37023923828bc86d1ca8bdc25516f304eb
Gnome67 <102388813+Gnome777@users.noreply.github.com>
2025-06-09 22:59:28 -0500

    super rod is better
    can catch one of 10 different pokemon

--- data/wild/super_rod.asm
+++ data/wild/super_rod.asm
@@ -1,47 +1,53 @@
 ; super rod encounters
 SuperRodData:
 	; map, fishing group
-	dbw PALLET_TOWN,         .Group1
-	dbw VIRIDIAN_CITY,       .Group1
-	dbw CERULEAN_CITY,       .Group1
-	dbw VERMILION_CITY,      .Group1
-	dbw CELADON_CITY,        .Group1
-	dbw FUCHSIA_CITY,        .Group1
-	dbw CINNABAR_ISLAND,     .Group1
-	dbw ROUTE_4,             .Group1
-	dbw ROUTE_6,             .Group1
-	dbw ROUTE_10,            .Group1
-	dbw ROUTE_11,            .Group1
-	dbw ROUTE_12,            .Group1
-	dbw ROUTE_13,            .Group1
-	dbw ROUTE_17,            .Group1
-	dbw ROUTE_18,            .Group1
-	dbw ROUTE_19,            .Group1
-	dbw ROUTE_20,            .Group1
-	dbw ROUTE_21,            .Group1
-	dbw ROUTE_22,            .Group1
-	dbw ROUTE_23,            .Group1
-	dbw ROUTE_24,            .Group1
-	dbw ROUTE_25,            .Group1
-	dbw CERULEAN_GYM,        .Group1
-	dbw VERMILION_DOCK,      .Group1
-	dbw SEAFOAM_ISLANDS_B3F, .Group1
-	dbw SEAFOAM_ISLANDS_B4F, .Group1
-	dbw SAFARI_ZONE_EAST,    .Group1
-	dbw SAFARI_ZONE_NORTH,   .Group1
-	dbw SAFARI_ZONE_WEST,    .Group1
-	dbw SAFARI_ZONE_CENTER,  .Group1
-	dbw CERULEAN_CAVE_2F,    .Group1
-	dbw CERULEAN_CAVE_B1F,   .Group1
-	dbw CERULEAN_CAVE_1F,    .Group1
+	dbw PALLET_TOWN,         .Group
+	dbw VIRIDIAN_CITY,       .Group
+	dbw CERULEAN_CITY,       .Group
+	dbw VERMILION_CITY,      .Group
+	dbw CELADON_CITY,        .Group
+	dbw FUCHSIA_CITY,        .Group
+	dbw CINNABAR_ISLAND,     .Group
+	dbw ROUTE_4,             .Group
+	dbw ROUTE_6,             .Group
+	dbw ROUTE_10,            .Group
+	dbw ROUTE_11,            .Group
+	dbw ROUTE_12,            .Group
+	dbw ROUTE_13,            .Group
+	dbw ROUTE_17,            .Group
+	dbw ROUTE_18,            .Group
+	dbw ROUTE_19,            .Group
+	dbw ROUTE_20,            .Group
+	dbw ROUTE_21,            .Group
+	dbw ROUTE_22,            .Group
+	dbw ROUTE_23,            .Group
+	dbw ROUTE_24,            .Group
+	dbw ROUTE_25,            .Group
+	dbw CERULEAN_GYM,        .Group
+	dbw VERMILION_DOCK,      .Group
+	dbw SEAFOAM_ISLANDS_B3F, .Group
+	dbw SEAFOAM_ISLANDS_B4F, .Group
+	dbw SAFARI_ZONE_EAST,    .Group
+	dbw SAFARI_ZONE_NORTH,   .Group
+	dbw SAFARI_ZONE_WEST,    .Group
+	dbw SAFARI_ZONE_CENTER,  .Group
+	dbw CERULEAN_CAVE_2F,    .Group
+	dbw CERULEAN_CAVE_B1F,   .Group
+	dbw CERULEAN_CAVE_1F,    .Group
 	db -1 ; end
 
 ; fishing groups
 ; number of monsters, followed by level/monster pairs
 
-.Group1:
-	db 4
-	db 25, STARYU
-	db 25, KRABBY
+.Group:
+	db 10
+	db 25, PSYDUCK
 	db 25, POLIWAG
+	db 25, TENTACOOL
+	db 25, SLOWPOKE
+	db 25, SEEL
+	db 25, SHELLDER
+	db 25, KRABBY
 	db 25, HORSEA
+	db 25, GOLDEEN
+	db 25, STARYU
